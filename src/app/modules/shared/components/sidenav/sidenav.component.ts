0e307cf8fcb99b6448f38e49449e3158133f60a8
ARONLS23 <aronsls20@gmail.com>
2024-09-26 15:17:12 -0500

    cerrar sesion

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,26 +1,34 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent implements OnInit {
 
   mobileQuery: MediaQueryList;
+  private keycloakService = inject(KeycloakService);
+
+  username: string = "";
 
   menuNav = [
-    { name: "Home", route: "home", icon: "home" },
-    { name: "Categorías", route: "category", icon: "category" },
-    { name: "Productos", route: "product", icon: "production_quantity_limits" }
+    { name: 'Home', route: 'home', icon: 'home' },
+    { name: 'Categorías', route: 'category', icon: 'category' },
+    { name: 'Productos', route: 'product', icon: 'production_quantity_limits' },
   ];
 
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
-   }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.username = this.keycloakService.getUsername();
+  }
+
+  logout() {
+    this.keycloakService.logout();
+  }
 }
