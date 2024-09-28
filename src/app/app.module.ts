85457fc9548b34ff608f2ff9a6018d62b5e5e27c
ARONLS23 <aronsls20@gmail.com>
2024-09-28 11:49:08 -0500

    agregando graficos al sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { NgChartsModule } from 'ng2-charts';
 
 function initializeKeycloak(keycloak: KeycloakService) {
   return () =>
@@ -31,7 +32,8 @@ function initializeKeycloak(keycloak: KeycloakService) {
     AppRoutingModule,
     DashboardModule,
     BrowserAnimationsModule,
-    KeycloakAngularModule
+    KeycloakAngularModule,
+    NgChartsModule
   ],
   providers: [
     {
--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { ProductElement } from 'src/app/modules/product/ProductElement';
+import { ProductService } from 'src/app/modules/shared/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  charBar: any;
+  chardoughnut: any;
+
+  private productService = inject(ProductService);
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.processProductsResponse(data);
+        console.log('rpta products', data);
+      },
+      error: (error) => {
+        console.log('Error', error);
+      }
+    });
+  }
+
+  processProductsResponse(resp: any) {
+    const nameProduct: string [] = [];
+    const account: number [] = [];
+
+    if (resp.metadata[0].code == "00") {
+      let listProduct = resp.productResponse.products;
+
+      listProduct.forEach((element: ProductElement) => {
+        nameProduct.push(element.name);
+        account.push(element.quantity);
+      });
+
+      //Gráfico de barras
+      this.charBar = new Chart('canvas-bar', {
+        type: 'bar',
+        data: {
+          labels: nameProduct,
+          datasets: [
+            {label: 'Productos', data: account}
+          ]
+        }
+      });
+
+      //Gráfico de doughnut
+      this.chardoughnut = new Chart('canvas-doughnut', {
+        type: 'doughnut',
+        data: {
+          labels: nameProduct,
+          datasets: [
+            {label: 'Productos', data: account}
+          ]
+        }
+      });
+    }
   }
 
 }
